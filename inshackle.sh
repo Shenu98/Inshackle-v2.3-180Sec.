0c24e6435680ae8be28138534993ff7cb86f847a
Shenu98 <91783648+Shenu98@users.noreply.github.com>
2022-01-05 21:25:46 -0500

    Update inshackle.sh

--- inshackle.sh
+++ inshackle.sh
@@ -467,8 +467,8 @@ fi
 sleep 3
 
 done
-printf "\e[1;31m[\e[0m\e[1;77m+\e[0m\e[1;31m]\e[0m\e[1;77m Sleeping 60 secs...\e[0m\n"
-sleep 60
+printf "\e[1;31m[\e[0m\e[1;77m+\e[0m\e[1;31m]\e[0m\e[1;77m Sleeping 180 secs...\e[0m\n"
+sleep 180
 #unfollow
 for celeb in $(cat celeb_id); do
 data='{"_uuid":"'$guid'", "_uid":"'$username_id'", "user_id":"'$celeb'", "_csrftoken":"'$var2'"}'
